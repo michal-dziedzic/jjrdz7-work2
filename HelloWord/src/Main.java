5e0fc2f9160059f4607a42936d39e790e05389b1
Michal Dziedzic <89808450+michal-dziedzic@users.noreply.github.com>
2022-08-07 16:27:34 +0200

    Update Main.java
    variable text added

--- HelloWord/src/Main.java
+++ HelloWord/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String text = "Hello Franek";
+        System.out.println(text);
     }
 }
